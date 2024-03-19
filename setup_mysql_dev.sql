b92ba0de46e46395edd2b47b3216a8a3690941cb
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-19 13:05:32 +0200

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE  IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
+CREATE DATABASE  IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY
 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

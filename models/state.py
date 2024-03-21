1ee84e30e6040d2b93b9d93ed38888c21dd7d784
osamanazar47 <osamanazar47@gmail.com>
2024-03-21 15:29:29 +0200

    state

--- models/state.py
+++ models/state.py
@@ -4,6 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 from models.city import City
+import models
+import shlex
 
 
 class State(BaseModel, Base):
@@ -12,3 +14,18 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all,delete, delete-orphan',
                           backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        city_list = []
+        result = []
+        for k in var:
+            c = k.replace('.', ' ')
+            c = shlex.split(c)
+            if (c[0] == "City"):
+                city_list.append(var[k])
+        for element in city_list:
+            if (element.state_id == self.id):
+                result.append(element)
+        return result

fa422edf573eb4fcfacd502b394c774d6b63c119
osamanazar47 <osamanazar47@gmail.com>
2024-03-21 17:06:23 +0200

    state

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -20,13 +20,17 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'\
-                                      .format(getenv("HBNB_MYSQL_USER"),
-                                              getenv("HBNB_MYSQL_PWD"),
-                                              getenv("HBNB_MYSQL_HOST"),
-                                              getenv("HBNB_MYSQL_DB")),
-                                              pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -60,7 +64,7 @@ class DBStorage:
     def delete(self, obj=None):
         """Deletes the obj from the current session"""
         if obj:
-            self._session.delete(obj)
+            self.session.delete(obj)
 
     def reload(self):
         """reload all tables"""

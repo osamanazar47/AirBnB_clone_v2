99f5839d301e0c62f79d42be5c5ef633e210caf8
osamanazar47 <osamanazar47@gmail.com>
2024-04-13 08:51:28 +0200

    setup_server

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A function that genarates a .tgz archive and """
+from datetime import datetime
+import os
+from fabric.api import local
+
+def do_pack():
+    """Defining the function do_pack which will archive all the contents
+    of the web_static directory"""
+
+    now = datetime.now()
+    times = now.strftime("%Y%m%d%H%M%S")  # Add a timestamp to the filename
+    name = "versions/web_static_{}.tgz".format(times)
+
+    os.makedirs("versions", exist_ok=True)
+    if local("tar -cvzf {} web_static".format(name)).failed:
+        return None
+    return name

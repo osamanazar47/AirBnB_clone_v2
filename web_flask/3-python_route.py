0f7f47cea977720a40c136258bb764a6e7ed8016
osamanazar47 <osamanazar47@gmail.com>
2024-04-20 12:37:25 +0200

    py_is_cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,6 +28,7 @@ def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 
+@app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
     """Displays Python followed by the value of the text variable"""

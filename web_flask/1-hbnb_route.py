911d677afe90355dc2a3fa178ac585497253351e
osamanazar47 <osamanazar47@gmail.com>
2024-04-20 12:10:18 +0200

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,8 +2,8 @@
 """Starts a Flask web application
 - the application listens on 0.0.0.0 port 5000
 Functtions:
-	hello: displays Hello HBNB
-	hbnb: displays HBNB
+hello: displays Hello HBNB
+hbnb: displays HBNB
 """
 from flask import Flask
 
@@ -14,6 +14,7 @@ app = Flask(__name__)
 def hello():
     """a function that displays Hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
